e3aebe25015c31f8fd4ef45cad9419a5761d6ecf
Ethan Borrowman <ethan_borrowman@hotmail.com>
2024-08-27 14:30:29 -0500

    Initial project commit

---
+++ DesertRecipeBrowser/DesertRecipeBrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesertRecipeBrowserApp.swift
+//  DesertRecipeBrowser
+//
+//  Created by Ethan Borrowman on 8/27/24.
+//
+
+import SwiftUI
+
+@main
+struct DesertRecipeBrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
